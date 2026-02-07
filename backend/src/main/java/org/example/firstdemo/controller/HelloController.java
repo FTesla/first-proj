43bea58aadf215d75221dab490420ab35dbbae50
FTesla <bfd007@mail.ru>
2026-02-07 21:22:56 +0100

    Update CI\CD

--- backend/src/main/java/org/example/firstdemo/controller/HelloController.java
+++ backend/src/main/java/org/example/firstdemo/controller/HelloController.java
@@ -5,9 +5,11 @@ import org.example.firstdemo.service.HelloService;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 @Scope("request")
 @CrossOrigin(origins = "http://localhost:4200")
 public class HelloController {
@@ -23,3 +25,4 @@ public class HelloController {
         return helloService.getHello();
     }
 }
+
